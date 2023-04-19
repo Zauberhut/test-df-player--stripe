f25746b9eecd7f58a8f2fa407e807318443f1bb5
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2023-04-19 23:28:08 +0200

    Reaktionszeit extrem verlangsamt

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ input.onButtonPressed(Button.A, function () {
     Informatiktheater.startbit_clearLight()
 })
 input.onButtonPressed(Button.AB, function () {
-    matrix.showText("hallo", 0, neopixel.colors(NeoPixelColors.Red))
+    matrix.scrollText("guten Tag ", 94, neopixel.colors(NeoPixelColors.Red))
 })
 input.onButtonPressed(Button.B, function () {
     dfplayermini.press(dfplayermini.playType.PlayNext)
@@ -27,4 +27,4 @@ dfplayermini.connect(SerialPin.P1, SerialPin.P2)
 dfplayermini.playFile(46, dfplayermini.isRepeat.No)
 Informatiktheater.startbit_setPixelRGBArgs(StartbitLights.Light1, StartbitRGBColors.Blue)
 dfplayermini.setVolume(28)
-matrix = neopixel.create_matrix(HiwonderPins.P16, matrixSizes.medium_32x8, PowerSource.Intern)
+matrix = neopixel.create_matrix(HiwonderPins.P16, matrixSizes.medium_32x8, PowerSource.Extern)
